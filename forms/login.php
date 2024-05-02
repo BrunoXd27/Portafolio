b6160fa3683c2f09e718cee12b8f46c372cf78d1
BrunoXd27 <brunoandre27jul@gmail.com>
2024-05-02 03:05:22 -0600

    La version con la pagina de log in no funcional

---
+++ forms/login.php
@@ -0,0 +1,9 @@
+<?php
+
+
+    if( file_exists($php_login_form = '../assets/vendor/php-login-form/php-login-form.php' )) {
+        include( $php_login_form );
+      } else {
+        die( 'Unable to load the "PHP Log in Form" Library!');
+      }
+?>
